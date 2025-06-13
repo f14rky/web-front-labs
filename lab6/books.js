c5a1e980c61524b08ccc57a0eff5a7eacf874c3b
iss <flarkyy@gmail.com>
2025-06-13 21:26:04 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,12 @@
+const books = [
+    { author: 'Джоан Роулинг', name: 'Гарри Поттер и философский камень', year: 1997, price: 700, bestseller: true, inStock: true },
+    { author: 'Джон Толкин', name: 'Властелин колец: Братство кольца', year: 1954, price: 650, bestseller: true, inStock: false },
+    { author: 'Рэй Брэдбери', name: '451° по Фаренгейту', year: 1953, price: 520, bestseller: false, inStock: true },
+    { author: 'Эрнест Хемингуэй', name: 'Старик и море', year: 1952, price: 470, bestseller: false, inStock: false },
+    { author: 'Агата Кристи', name: 'Убийство в Восточном экспрессе', year: 1934, price: 430, bestseller: true, inStock: true },
+    { author: 'Стивен Кинг', name: 'Оно', year: 1986, price: 580, bestseller: true, inStock: true },
+    { author: 'Харпер Ли', name: 'Убить пересмешника', year: 1960, price: 490, bestseller: false, inStock: true },
+    { author: 'Дэн Браун', name: 'Код да Винчи', year: 2003, price: 620, bestseller: true, inStock: false },
+    { author: 'Джейн Остин', name: 'Гордость и предубеждение', year: 1813, price: 390, bestseller: false, inStock: true },
+    { author: 'Габриэль Гарсиа Маркес', name: 'Сто лет одиночества', year: 1967, price: 540, bestseller: true, inStock: true }
+];
